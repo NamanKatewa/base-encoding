634529724822c05bfe1a7ec65624c1a2ae5692ae
Naman Katewa <85558238+NamanKatewa@users.noreply.github.com>
2024-07-11 11:36:27 +0530

    base 64 functionality complete

---
+++ base64/base64.js
@@ -0,0 +1,86 @@
+const characterSet =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+function encode(input) {
+  let binaryString = "";
+
+  for (let i = 0; i < input.length; i++) {
+    const binaryChar = input.charCodeAt(i).toString(2).padStart(8, "0");
+    binaryString += binaryChar;
+  }
+
+  let encodedString = "";
+  for (let i = 0; i < binaryString.length; i += 6) {
+    const binarySegment = binaryString.substring(i, i + 6).padEnd(6, "0");
+    const index = parseInt(binarySegment, 2);
+    encodedString += characterSet[index];
+  }
+
+  while (encodedString.length % 4 !== 0) {
+    encodedString += "=";
+  }
+
+  return encodedString;
+}
+
+function decode(input) {
+  let binaryString = "";
+
+  input = input.replace(/=+$/, "");
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input.charAt(i);
+    const binarySegment = characterSet
+      .indexOf(char)
+      .toString(2)
+      .padStart(6, "0");
+    binaryString += binarySegment;
+  }
+
+  let decodedString = "";
+  for (let i = 0; i < binaryString.length; i += 8) {
+    const byte = binaryString.substring(i, i + 8);
+    if (byte.length === 8) {
+      const charCode = parseInt(byte, 2);
+      decodedString += String.fromCharCode(charCode);
+    }
+  }
+
+  return decodedString;
+}
+
+let option = "encode";
+
+const encodeButton = document.getElementById("encode");
+
+const decodeButton = document.getElementById("decode");
+
+const form = document.getElementById("form");
+
+const output = document.getElementById("output");
+
+encodeButton.addEventListener("click", function () {
+  option = "encode";
+  encodeButton.classList.add("selected");
+  decodeButton.classList.remove("selected");
+});
+
+decodeButton.addEventListener("click", function () {
+  option = "decode";
+  decodeButton.classList.add("selected");
+  encodeButton.classList.remove("selected");
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const input = document.getElementById("input").value;
+
+  if (option === "encode") {
+    const res = encode(input);
+    output.textContent = res;
+  } else if (option === "decode") {
+    const res = decode(input);
+    output.textContent = res;
+  }
+});
